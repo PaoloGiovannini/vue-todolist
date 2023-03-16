de518ffd2119d49a0f44f372bf1307a7ab8687f8
Paolo Giovannini <giovannini13@gmail.com>
2023-03-16 14:30:42 +0100

    bonus 1 completato

---
+++ bonus/js/main.js
@@ -0,0 +1,33 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        newToDo: '',
+        toDoList:[
+            {
+                text:'Andare a fare la spesa',
+                done:false
+            },
+            {
+                text:'Comprare la ps5',
+                done: false
+            },
+            {
+                text:'Giocare a pallone',
+                done:false
+            }
+            
+        ] 
+      }
+    },
+    methods:{
+        removeToDo(index){
+            this.toDoList.splice(index, 1);
+        },
+        addToDo(){
+            this.toDoList.push({text: this.newToDo, done: false})
+            this.newToDo = ''
+        }
+    }
+  }).mount('#app')
